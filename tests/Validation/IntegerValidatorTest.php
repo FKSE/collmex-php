6e3e7dc6455dda1b32538d28f35630928125c03b
Frido Koch <info@fridokoch.de>
2015-02-01 11:02:22 +0100

    Added test for integer validator

--- tests/Validation/IntegerValidatorTest.php
+++ tests/Validation/IntegerValidatorTest.php
@@ -1,9 +1,31 @@
 <?php
-
-
 namespace FKSE\Collmex\Test\Validation;
 
+use FKSE\Collmex\Validation\IntegerValidator;
 
-class IntegerValidatorTest extends \PHPUnit_Framework_TestCase {
-
+/**
+ *  Test IntegerValidator
+ *
+ * @author Fridolin Koch <info@fridokoch.de>
+ */
+class IntegerValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testValidate()
+    {
+        $validator = new IntegerValidator();
+        //success
+        $this->assertTrue($validator->validate('1234'));
+        $this->assertTrue($validator->validate('4235'));
+        $this->assertTrue($validator->validate('1'));
+        $this->assertTrue($validator->validate('12'));
+        $this->assertTrue($validator->validate('001'));
+        //fail
+        $this->assertFalse($validator->validate('1,2'));
+        $this->assertFalse($validator->validate('1.2'));
+        $this->assertFalse($validator->validate('.2'));
+        $this->assertFalse($validator->validate('asf'));
+    }
 }
